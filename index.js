932c3116a31f5dc238caee6e17dd8696bfac9cdb
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-04-14 07:20:49 +0200

    made the posts appear in DOM

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const table = document.querySelector("#users");
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((result) => {
-    console.log(users);
     users = result;
     for (let i = 0; i < users.length; i++) {
       CreateTable(users[i].name, i + 1);
@@ -16,22 +15,34 @@ function CreateTable(name, id) {
   let userName = document.createElement("td");
   userName.textContent = name;
   tr.appendChild(userName);
-  tr.appendChild(createBtn(id));
+  tr.appendChild(createBtn(id, name));
 }
 
-function createBtn(id) {
+function createBtn(id, name) {
   let btn = document.createElement("button");
-  btn.id = `GetPosts-${id}`;
+  btn.className = `GetPosts`;
   btn.addEventListener("click", () => {
     getPosts(id);
   });
-  btn.textContent = "Show Posts";
+  btn.textContent = "Show Posts of " + name;
   return btn;
 }
 function getPosts(id) {
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
     .then((result) => {
-      console.log(result.filter((post) => post.userId === id));
+      let container = document.createElement("section");
+      document.body.appendChild(container);
+      let R = result.filter((post) => post.userId === id);
+      console.log(R);
+      R.forEach((post) => {
+        let title = document.createElement("h3");
+        title.textContent = post.title;
+        container.appendChild(title);
+        let body = document.createElement("p");
+        body.textContent = post.body;
+        container.appendChild(body);
+      });
     });
 }
+function makeContainer() {}
